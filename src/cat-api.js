f58bd9a2162cf72c2112cda07576d82e75b2ee29
JuliaLuk <julia1lukasiewicz@gmail.com>
2023-12-02 10:32:41 -0600

    fixed function

--- src/cat-api.js
+++ src/cat-api.js
@@ -1,11 +1,16 @@
-fetchBreeds('https://api.thecatapi.com/v1/breeds')
-  .then(result => {
-    if (!result.ok) {
-      throw new Error('Oops! Something went wrong! Try reloading the page!');
-    }
-    return result.json();
-  })
-  .then(data => console.log(data))
-  .catch(error => console.log(error));
+// import axios from 'axios';
 
-export function fetchBreeds() {}
+function fetchBreeds() {
+  return axios
+    .get('https://api.thecatapi.com/v1/breeds')
+    .then(result => {
+      if (!result.ok) {
+        throw new Error('Oops! Something went wrong! Try reloading the page!');
+      }
+      return result.json();
+    })
+    .then(data => console.log(data))
+    .catch(error => console.log(error));
+}
+
+export { fetchBreeds };
